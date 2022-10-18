03724410781ab4dda8274c05d88c927bd63073a7
Grisell Diaz Leines <gdiazleines@t37907t2mp.windows.ebi.ac.uk>
2022-10-18 09:31:23 +0100

    faster creation of json files

--- pisa_utils/utils.py
+++ pisa_utils/utils.py
@@ -1,8 +1,8 @@
 import logging
 import os
 import os.path
-#from lxml import etree 
-import xml.etree.ElementTree as ET
+from lxml import etree 
+#import xml.etree.ElementTree as ET
 
 
 from gemmi import cif
@@ -21,8 +21,7 @@ def parse_xml_file(xml_file):
     if xml_exists:
 
         logging.debug("parsing: {}".format(xml_file))
-        #tree = etree.parse(xml_file)
-        tree = ET.parse(xml_file)
+        tree = etree.parse(xml_file)
         root = tree.getroot()
 
     return root
@@ -50,7 +49,7 @@ def create_pisa_config(dataroot, setup_dir):
 
 
 def read_uniprot_info(
-    int_lab_seqnum, int_seqnum, int_atname, int_resname, pdb_id, input_updated_cif
+        int_lab_seqnum, int_seqnum, int_atname, int_resname, pdb_id, updated_cif_block
 ):
     """
     Function reads atom's UniProt accession numbers and sequence numbers
@@ -61,103 +60,64 @@ def read_uniprot_info(
     :param int_atname: type str - atom name
     :param int_resname: type str - atom residue name
     :param pdb_id: type str - PDB id
-    :param input_updated_cif: type str - path to updated cif file
+    :param parsed updated cif data: type str - path to updated cif file
     :return: (atom uniprot accession number, atom uniprot sequence number)
     """
+    
     unp_acc = None
     unp_num = None
 
-    if not (input_updated_cif and os.path.exists(input_updated_cif)):
-        #logging.info(f"Updated CIF [{input_updated_cif}] is not provided or is invalid")
-        return unp_acc, unp_num
-
-    # updated cif file path
-    # path = os.path.join(input_updated_cif, "{}_updated.cif".format(pdb_id))
-    path = input_updated_cif
-
-    # Reading UniProt acc and seq numbers in updated cif file:
-    doc = cif.read(path)
-    block = doc.sole_block()
-    db_seq_id = block.find_loop("_pdbx_sifts_xref_db.seq_id")
-    db_acc = block.find_loop("_pdbx_sifts_xref_db.unp_acc")
-    db_num = block.find_loop("_pdbx_sifts_xref_db.unp_num")
-    res_name = block.find_loop("_pdbx_sifts_xref_db.mon_id")
-    #label_seq_id = block.find_loop("_atom_site.label_seq_id")
-    #atom_name = block.find_loop("_atom_site.label_atom_id")
-    #auth_seq_id = block.find_loop("_atom_site.auth_seq_id")
-    #db_name = block.find_loop("_atom_site.pdbx_sifts_xref_db_name")
-    #db_acc = block.find_loop("_atom_site.pdbx_sifts_xref_db_acc")
-    #db_num = block.find_loop("_atom_site.pdbx_sifts_xref_db_num")
-    #res_name = block.find_loop("_atom_site.label_comp_id")
-
-    # counts if atom is not found in updated cif file
+    # counts if atom is not found in updated cif file                                                                
     n = 0
 
-    # if sequence identifier read in pisa-lite is not available,
-    # replace with a dot
+    # if sequence identifier read in pisa-lite is not available,                                                    
+    # replace with a dot                                                                                             
     if int(int_lab_seqnum) < 0:
         int_lab_seqnum = "."
 
-    # Search atom in updated cif file and read uniprot numbers
-
-    for (labseqid, resname, dbacc, dbnum) in zip(
-        db_seq_id, res_name, db_acc, db_num
-    ):
-        #print(labseqid, dbacc)
-        
-        if (
-            labseqid == int_lab_seqnum.strip()
-            and resname == int_resname.strip()
-        ):
-            n += 1
-            if labseqid != ".":
-                unp_acc = dbacc
-                unp_num = dbnum
-            
-                return unp_acc, unp_num
-        
-            else:
-                logging.debug("No UNP numbers found for atom:")
-                logging.debug(
-                    "name {},label_seq_id {},seq_num {}, residue {}".format(
-                        int_atname, int_lab_seqnum, int_seqnum, int_resname
-                    )
-                )
-                unp_acc = None
-                unp_num = None
-                return unp_acc, unp_num
-    """
-    for (labseqnum, seqnum, name, resname, dbname, dbacc, dbnum) in zip(
-        label_seq_id, auth_seq_id, atom_name, res_name, db_name, db_acc, db_num
-    ):
-        if (
-            labseqnum == int_lab_seqnum.strip()
-            and seqnum == int_seqnum.strip()
-            and name == int_atname.strip()
-            and resname == int_resname.strip()
-        ):
-            n += 1
-            if dbname == "UNP":
-                unp_acc = dbacc
-                unp_num = dbnum
-
-                return unp_acc, unp_num
-            else:
-                logging.debug("No UNP numbers found for atom:")
-                logging.debug(
-                    "name {},label_seq_id {},seq_num {}, residue {}".format(
-                        int_atname, int_lab_seqnum, int_seqnum, int_resname
-                    )
-                )
-                unp_acc = None
-                unp_num = None
-                return unp_acc, unp_num
-    if n == 0:  # If atom was not found in updated cif file, return message
-        logging.debug("atom not found in updated cif file:")
-        logging.debug(
-            "name {},label_seq_id {},seq_num {}, residue {}".format(
-                int_atname, int_lab_seqnum, int_seqnum, int_resname
-            )
-        )
+    if not (updated_cif_block):
         return unp_acc, unp_num
-    """
+
+    else:
+        db_seq_id = updated_cif_block.find_values("_pdbx_sifts_xref_db.seq_id")
+        db_acc = updated_cif_block.find_values("_pdbx_sifts_xref_db.unp_acc")
+        db_num = updated_cif_block.find_values("_pdbx_sifts_xref_db.unp_num")
+        res_name = updated_cif_block.find_values("_pdbx_sifts_xref_db.mon_id")
+
+
+        for (labseqid, resname, dbacc, dbnum) in zip(
+                db_seq_id, res_name, db_acc, db_num
+        ):
+            
+            if (
+                    labseqid == int_lab_seqnum.strip()
+                    and resname == int_resname.strip()
+            ):
+            
+                n += 1
+                if labseqid != ".":
+                    unp_acc = dbacc
+                    unp_num = dbnum
+            
+                    return unp_acc, unp_num
+        
+                else:
+                    logging.debug("No UNP numbers found for atom:")
+                    logging.debug(
+                        "name {},label_seq_id {},seq_num {}, residue {}".format(
+                            int_atname, int_lab_seqnum, int_seqnum, int_resname
+                        )
+                    )
+                    unp_acc = None
+                    unp_num = None
+                    return unp_acc, unp_num
+        if n == 0:  # If residue was not found in updated cif file, return message                                              
+            logging.debug("residue not found in updated cif file:")                                                             
+            logging.debug(                                                                                                   
+                "name {},label_seq_id {},seq_num {}, residue {}".format(                                                     
+                    int_atname, int_lab_seqnum, int_seqnum, int_resname                                                      
+                )                                                                                                            
+            )                                                                                                                
+            return unp_acc, unp_num
+
+    
