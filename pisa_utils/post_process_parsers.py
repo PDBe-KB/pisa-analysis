629bd4b46caac420fd7bd8f9b339c705a8647828
jellaway <jellaway@ebi.ac.uk>
2026-05-26 13:01:48 +0100

    Parser for generating interface details file

--- pisa_utils/post_process_parsers.py
+++ pisa_utils/post_process_parsers.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
 import json
 import logging
+import os
 
 from pisa_utils.models.post_process_models import (
+    InterfaceDetails,
+    InterfaceDetailsComponent,
+    InterfaceDetailsList,
     PQSSetRow,
     ComplexTableRow,
     ComplexTable,
@@ -14,7 +18,9 @@ LOGGER = logging.getLogger(__name__)
 
 
 class PostProcessor(ABC):
-    def __init__(self, input_json_path, output_json_path, compressed=False):
+    def __init__(
+        self, input_json_path: str, output_json_path: str, compressed: bool = False
+    ):
         self.input_json_path = input_json_path
         self.output_json_path = output_json_path
         self.compressed = compressed
@@ -101,3 +107,124 @@ class PostProcessComplexTable(PostProcessor):
         complex_table = ComplexTable(output).model_dump()
 
         self.save(self.output_json_path, complex_table)
+
+
+class PostProcessInterfaceDetailsList(PostProcessor):
+    def __init__(
+        self,
+        path_interfaces: str,
+        path_monomers_json: str,
+        output_json_path: str,
+        compressed: bool = False,
+    ):
+        super().__init__(
+            input_json_path=path_monomers_json,
+            output_json_path=output_json_path,
+            compressed=compressed,
+        )
+        self.path_interfaces = path_interfaces
+
+    def _extract_monomer_data(
+        self, component_id: str, interface_auth_asym_id: str = None
+    ) -> dict:
+        """
+        Extract monomer (component) data for a given component ID
+
+        :param component_id: ID of the component to extract data for. Not auth_asym_id
+        :type component_id: str
+        :param interface_auth_asym_id: auth_asym_id from the interface.json file. File
+            provenance differs from monomers_extended.json, so used for sanity checks.
+            Defaults to None
+        :type interface_auth_asym_id: str, optional
+        :raises ValueError: If no monomer data is found
+        :return: Component (monomer) data for the specified component ID
+        :rtype: dict
+        """
+
+        monomer_data_filtered = next(
+            (
+                item
+                for item in self.data["components"]
+                if item["component_id"] == component_id
+            ),
+            None,
+        )
+
+        # Sanity checks
+        if not monomer_data_filtered:
+            LOGGER.error(f"No monomer data found for component ID: {component_id}")
+            raise ValueError(f"No monomer data found for component ID: {component_id}")
+
+        if interface_auth_asym_id:
+            assert monomer_data_filtered["auth_asym_id"] == interface_auth_asym_id, (
+                f"Component ID mismatch: interface says {interface_auth_asym_id}, "
+                f"but monomer data is {monomer_data_filtered['auth_asym_id']}"
+            )
+
+        return monomer_data_filtered
+
+    def parse(self):
+        super().parse()
+
+        output = []
+
+        # Iterate over interfaces JSONs
+        for interface_json_file in os.listdir(self.path_interfaces):
+            LOGGER.info(f"Processing interface JSON file: {interface_json_file}")
+
+            with open_compressed(
+                os.path.join(self.path_interfaces, interface_json_file),
+                compressed=self.compressed,
+            ) as f:
+                interface_data = json.load(f)
+
+            # Interface level data
+            interface_id = interface_data["interface_id"]
+            interface = interface_data["interface"]
+            int_type = interface["int_type"]
+            css = interface["css"]
+
+            components: list[InterfaceDetailsComponent] = []
+
+            for molecule in interface.get("molecules", []):
+                auth_asym_id = molecule["auth_asym_id"]
+                component_id = molecule["component_id"]
+
+                LOGGER.info(f"Processing molecule with component_id: {component_id}")
+
+                monomer_data_filtered = self._extract_monomer_data(
+                    component_id=component_id, interface_auth_asym_id=auth_asym_id
+                )
+
+                # Component-level data
+                component = InterfaceDetailsComponent(
+                    auth_asym_id=auth_asym_id,
+                    molecule_class=molecule["molecule_class"],
+                    symmetry_operation=molecule["symmetry_operation"],
+                    symmetry_id=molecule["symmetry_id"],
+                    int_natoms=molecule["int_natoms"],
+                    surface_natoms=monomer_data_filtered["n_surface_atoms"],
+                    total_atoms=monomer_data_filtered["total_atoms"],
+                    int_nres=molecule["int_nres"],
+                    surface_nres=monomer_data_filtered["n_surface_residues"],
+                    total_residues=monomer_data_filtered["total_residues"],
+                    int_area=molecule["int_area"],
+                    surface_area=monomer_data_filtered["asa"],
+                    solv_energy=monomer_data_filtered.get("solv_energy", None),
+                    int_solv_energy=molecule["int_solv_energy"],
+                    pvalue=molecule["pvalue"],
+                )
+                components.append(component)
+
+            # Add interface details to ordered list
+            interface_details = InterfaceDetails(
+                interface_id=interface_id,
+                int_type=int_type,
+                css=css,
+                components=components,
+            )
+            output.append(interface_details)
+
+        interface_details_list = InterfaceDetailsList(output).model_dump()
+
+        self.save(self.output_json_path, interface_details_list)
